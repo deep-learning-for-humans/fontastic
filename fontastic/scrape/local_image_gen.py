308d53e26d5fa82ea41e705bdecc1edf0d71b886
Raghotham <raghotham.s@gmail.com>
2019-02-02 13:20:40 +0530

    Modularizing image generation

--- fontastic/scrape/local_image_gen.py
+++ fontastic/scrape/local_image_gen.py
@@ -10,68 +10,72 @@ fontSize = 40
 imgSize = (1200,50)
 position = (0,0)
 
-#All images will be stored in 'Synthetic_dataset' directory under current directory
-dataset_path = os.path.join (os.getcwd(), 'synthetic_dataset')
-if not os.path.exists(dataset_path):
-    os.makedirs(dataset_path)
 
-lower_case_list = list(string.ascii_lowercase)
-upper_case_list = list(string.ascii_uppercase)
-digits = range(0,10)
+def generate_font_images(font, ttf_path):
+    print(os.path.join (ttf_path, '*.ttf'))
+    font_files = glob.glob(os.path.join (ttf_path, font, '*.ttf'))
+    print(font_files)
+    print(f'found {len(font_files)} font file for {font}')
 
-digits_list=[]
-for d in digits:
-    digits_list.append(str(d))
+    dst_img_path = os.path.join(dataset_path, font)
+    if not os.path.exists(dst_img_path):
+        os.makedirs(dst_img_path)
+        print(f'creating destination folder for image {font}')
 
-all_char_list = lower_case_list + upper_case_list + digits_list
+    for font_var in font_files:
+        #print "Checking "+p
+        font_file = ntpath.basename(font_var)
+        font_file = font_file.rsplit('.')
+        font_file = font_file[0]
 
-fonts_list = ['Roboto']
-# for line in fhandle:
-#    fonts_list.append(line.rstrip('\n'))
+        # font_var = font_var.lower()
+        #Check desired font
+        #   if f_lower in s_lower:
+        font_ttf = ImageFont.truetype(font_var, fontSize)
 
-total_fonts = len(fonts_list)
-#paths = ttfquery.findsystem.findFonts()
-all_fonts = glob.glob(os.path.join (os.getcwd(), 'data', 'fonts', 'roboto', '*.ttf'))
-# all_fonts = glob.glob("C:\\Windows\\Fonts\\*.ttf")
-f_flag = np.zeros(total_fonts)
+        text_types = ["""the quick brown fox jumped the lazy dog""",
+                """a b c d e f g h i j k l m n o p q r s t u v w x y z""",
+                """1 2 3 4 5 6 7 8 9 0"""
+                ]
 
-print(all_fonts) 
+        for idx, ch in enumerate(text_types):
+            image = Image.new("RGB", imgSize, (255,255,255))
+            draw = ImageDraw.Draw(image)
+            pos_x = 0
+            pos_y = 0
 
-for sys_font in all_fonts:
-    #print "Checking "+p
-   font_file = ntpath.basename(sys_font)
-   font_file = font_file.rsplit('.')
-   font_file = font_file[0]
-   f_idx = 0
-   for font in fonts_list:
-    #   f_lower = font.lower()
-      s_lower = sys_font.lower()
-      #Check desired font
-    #   if f_lower in s_lower:
-      path = sys_font
-      font_ttf = ImageFont.truetype(path, fontSize)
-      f_flag[f_idx] = 1
+            position = (pos_x, pos_y)
+            print(font)
+            print(font_ttf)
+            print(font_var)
+            draw.text(position, ch, (0,0,0), font=font_ttf)
 
-      text_types = ["""the quick brown fox jumped the lazy dog""",
-              """a b c d e f g h i j k l m n o p q r s t u v w x y z""",
-              """1 2 3 4 5 6 7 8 9 0"""
-              ]
+            file_name = font_file + '_' + str(idx) + '_' + '.jpg'
+            file_name = os.path.join(dataset_path, font, file_name)
+            image.save(file_name, quality=95, dpi=(600,600))
 
-      for idx, ch in enumerate(text_types):
-          image = Image.new("RGB", imgSize, (255,255,255))
-          draw = ImageDraw.Draw(image)
-          pos_x = 0
-          pos_y = 0
-          pos_idx=0
-        #   for y in [pos_y-1, pos_y, pos_y+1]:
-        #       for x in [pos_x-1, pos_x, pos_x+1]:
-          position = (pos_x, pos_y)
-          print(font)
-          print(font_ttf)
-          draw.text(position, ch, (0,0,0), font=font_ttf)
+if __name__ == '__main__':
+    
+    # TODO better logging
+    # bootstrapping stuff
+    # TODO cleanup later!
+    ttf_path = os.path.join (os.getcwd(), 'data', 'src', 'fonts')
+    print(ttf_path)
+    dataset_path = os.path.join (os.getcwd(), 'data', 'dst')
+    if not os.path.exists(dataset_path):
+        os.makedirs(dataset_path)
 
-          file_name = font_file + '_' + str(idx) + '_' + '.jpg'
-          file_name = os.path.join(dataset_path,file_name)
-          image.save(file_name, quality=95, dpi=(600,600))
-          pos_idx = pos_idx + 1
-      f_idx = f_idx + 1
+    if not os.path.exists(ttf_path):
+        print('No fonts files found!')
+        exit(-1)
+
+    fonts_list = os.listdir(ttf_path)
+
+    total_fonts = len(fonts_list)
+    all_fonts = glob.glob(os.path.join (os.getcwd(), 'data', 'fonts', 'roboto', '*.ttf'))
+
+    print(all_fonts) 
+
+    # TODO more clean here with params
+    generate_font_images('roboto', ttf_path)
+    generate_font_images('open-sans', ttf_path)
